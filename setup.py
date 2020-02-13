ce9ae46a6fd125a3c74631deac02e2260e50cfa2
dirkwind <dirkwind707@gmail.com>
2020-02-12 20:16:17 -0600

    add setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = []
+
+setup(
+    name="progbar",
+    version="0.0.1",
+    author="Harry Heitmeier",
+    author_email="dirkwind707@gmail.com",
+    description="A simple progress bar.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dirkwind/progbar/",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7.3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
